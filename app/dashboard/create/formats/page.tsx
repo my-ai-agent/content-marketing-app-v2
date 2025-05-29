5b3fb67fb973d646ca56b395b678822dbca2126c
Michael Gibbons <mike.gibbo@xtra.co.nz>
2025-05-29 14:15:35 +1200

    Update page.tsx Fix formats page import for plans config
    Fix formats page import for plans config
    
    - Move getPlanLimits import to top level (out of function)
    - Correct import path to '../../../config/plans'
    - Complete centralization fixes for all pages

--- app/dashboard/create/formats/page.tsx
+++ app/dashboard/create/formats/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { useState, useEffect } from 'react'
 
 export default function Formats() {
-  import { getPlanLimits } from '../../../../../../config/plans';
+  import { getPlanLimits } from '../../../config/plans';
   const userPlan = 'free'; // TODO: Get from user data
   const planLimits = getPlanLimits(userPlan);
     { name: 'Social Media Posts', icon: '📱', desc: 'Instagram, Facebook, Twitter, LinkedIn' },
