32bc8e012925eb787ecd6511213c39c2ed6141bc
Michael Gibbons <mike.gibbo@xtra.co.nz>
2025-06-02 12:36:23 +1200

    Update layout.tsx Remove flex items-center justify-center from body.
    Remove flex items-center justify-center from body.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen min-w-screen flex items-center justify-center">
+      <body className="min-h-screen min-w-screen bg-white">
         {children}
       </body>
     </html>
